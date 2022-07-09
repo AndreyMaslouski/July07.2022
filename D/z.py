927754b94d1b64b2d44e2a183c8e0473a197974f
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-10 02:06:57 +0300

    Ввел переменную блок. Прикрепил к ней картинку коричневого квадрата. Вставил его в окно Игры.

--- D/z.py
+++ D/z.py
@@ -1,15 +1,36 @@
 import pygame
 import time
+from pygame.locals import *
 
 
 if __name__ == "__main__":
     pygame.init()
 
     surface = pygame.display.set_mode((1000,500))
-    surface.fill((255,255,255))
+    surface.fill((110,110,5))
+
+    block = pygame.image.load("resourses/block.jpg").convert()
+    block_x = 100
+    block_y = 100
+
+    surface.blit(block,(block_x,block_y))
+
     pygame.display.flip()
 
     time.sleep(5)
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running == False
+            elif event.type ==QUIT:
+                running == False
+
+
+
 
 
 
