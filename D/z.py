1b3a7bedd83997ef7eea8fc1f281338c44c7e261
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-10 22:34:57 +0300

    Записал класс Змеи. Добавил в него родительский, со строчками кода, требующихся для её создания.

--- D/z.py
+++ D/z.py
@@ -2,6 +2,26 @@ import pygame
 import time
 from pygame.locals import *
 
+class Snake:
+    def __init__(self,parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resourses/block.jpg").convert()
+        self_x = 100
+        self_y = 100
+        block_x,block_y = 100,100
+
+    def draw(self):
+        self.parent_screen.blit(self.block,(self.x,self.y))
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((500,500))
+        self.surface.fill((110,110,5))
+
+    def run(self):
+        pass
+
 def draw_block():
     surface.fill((110,110,5))
     surface.blit(block,(block_x,block_y))
@@ -9,16 +29,19 @@ def draw_block():
 
 
 if __name__ == "__main__":
+    game = Game()
+    game.run()
+
+
+
+
     pygame.init()
+
 
     surface = pygame.display.set_mode((1000,500))
     surface.fill((110,110,5))
 
-    block = pygame.image.load("resourses/block.jpg").convert()
-    block_x = 100
-    block_y = 100
 
-    surface.blit(block,(block_x,block_y))
 
     pygame.display.flip()
 
