b1aae31ad685fccd1f0c5c84fb455d81ec96655d
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-13 00:50:44 +0300

    Добавил в Snake увеличение её длины при каждом съеденном яблоке. А также дисплей подсчета очков. Но пока он что-то не работает. Попытаюсь выяснить причину этому.

--- D/z.py
+++ D/z.py
@@ -18,8 +18,8 @@ class Apple:
         pygame.display.flip()
 
     def move(self):
-        self.x = random.randint(0,25)*SIZE
-        self.y = random.randint(0,20)*SIZE
+        self.x = random.randint(0,24)*SIZE
+        self.y = random.randint(0,15)*SIZE
 
 class Snake:
     def __init__(self, parent_screen, length):
@@ -29,6 +29,11 @@ class Snake:
         self.x = [SIZE]*length
         self.y = [SIZE]*length
         self.direction = 'down'
+
+    def increase_length(self):
+        self.length +=1
+        self.x.append(-1)
+        self.y.append(-1)
 
     def move_left(self):
         self.direction = 'left'
@@ -71,24 +76,32 @@ class Snake:
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000, 800))
-        self.snake = Snake(self.surface,6)
+        self.surface = pygame.display.set_mode((500, 500))
+        self.snake = Snake(self.surface,3)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
 
     def is_collision(self,x1,y1,x2,y2):
-        if x1 >= x2 and x1 <= x2 + SIZE:
-            if y1 >= y2 and y1 <= y2 + SIZE:
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
                 return True
         return False
 
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
 
         if self.is_collision(self.snake.x[0],self.snake.y[0],self.apple.x,self.apple.y):
+            self.snake.increase_length()
             self.apple.move()
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial',30)
+        score = font.render(f"Score: {self.snake.length}",True,(200,200,200))
+        self.surface.blit(score,(800,10))
 
     def run(self):
         running = True
@@ -97,7 +110,7 @@ class Game:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
-                        running == False
+                        running = False
                     if event.key == K_UP:
                         self.snake.move_up()
                     if event.key == K_DOWN:
