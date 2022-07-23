d9175c36e78b6681f986a676b3010fb720a5d332
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-24 01:43:04 +0300

    -

---
+++ Telegram_Bot/1.py
@@ -0,0 +1,8 @@
+import telebot
+from telebot import types
+
+# Tokken = 5546376547:AAHgf4V3rsTgWPBk-6CKPvIOJLk_bQubzKg
+
+token= '5546376547:AAHgf4V3rsTgWPBk-6CKPvIOJLk_bQubzKg'
+bot = telebot.TeleBot(token)
+
