64e1a714570a82979d5f129a28c5338b4312867e
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-26 01:49:28 +0300

    Задал параметры шарику: его цвет, внешний вид, его скорость в полёте и перемещение. Пока улетает за пределы экрана, дописываю к нему код дальше.

--- D/Arkanoid/game_1.py
+++ D/Arkanoid/game_1.py
@@ -9,6 +9,12 @@ paddle_w = 300
 paddle_h = 35
 paddle_speed = 15
 paddle = pygame.Rect(WIDTH // 2 - paddle_w//2,HEIGHT - paddle_h - 10, paddle_w, paddle_h)
+#ball settings
+ball_radius = 20
+ball_speed = 6
+ball_rect = int(str(ball_radius) * 2 ** 0,5)
+ball = pygame.Rect(rnd(ball_rect, WIDTH - ball_rect), HEIGHT // 2,ball_rect,ball_rect)
+dx, dy = 1,-1
 
 pygame.init()
 sc = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -24,6 +30,10 @@ while True:
     sc.blit(img,(0,0))
     #drawing world
     pygame.draw.rect(sc,pygame.Color('darkorange'), paddle)
+    pygame.draw.circle(sc, pygame.Color('white'), ball.center, ball_radius)
+    # ball movement
+    ball.x += ball_speed * dx
+    ball.y += ball_speed * dy
     # control
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] and paddle.left > 0:
