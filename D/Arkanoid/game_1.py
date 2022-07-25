18b1e3174028779b50e5031881702fac39cffd5b
Andrei Mas <masloyskiandrey@gmail.com>
2022-07-26 01:15:54 +0300

    Добавил картинку на фон Игры.

--- D/Arkanoid/game_1.py
+++ D/Arkanoid/game_1.py
@@ -7,10 +7,14 @@ pygame.init()
 sc = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
+#background image
+img = pygame.image.load('1.jpg').convert()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+    sc.blit(img,(0,0))
 
     # update screen
     pygame.display.flip()
